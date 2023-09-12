77d69e7788891046505f9dcb68868dd41e639217
FL33TW00D <fleetwoodpersonal@gmail.com>
2023-09-12 10:32:44 +0300

    chore: transpile

--- playground/next.config.js
+++ playground/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    transpilePackages: ["@rumbl/laserbeak"],
-    optimizeFonts: false,
+    transpilePackages: ["whisper-turbo"],
 };
 
 module.exports = nextConfig;
