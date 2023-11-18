0928aa8ef54ed8272cd717ec354220493e9c1cc0
FL33TW00D <fleetwoodpersonal@gmail.com>
2023-11-18 21:50:44 +0700

    chore: reexport Task

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { InferenceSession } from "./inferenceSession";
 export { AvailableModels, ModelSizes } from "./models";
 export { SessionManager } from "./sessionManager";
 export { MicRecorder } from "./audio";
-export { DecodingOptionsBuilder, Segment, default as initialize} from "whisper-webgpu";
+export { DecodingOptionsBuilder, Segment, default as initialize, Task } from "whisper-webgpu";
